cd5e99b9be99cd73216c5d324dec13d63b254777
IFTE-13 <ifte.phoenix@gmail.com>
2023-08-04 21:10:40 +0600

    Testimonial Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import Contact from './Contact'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import Newsletter from './Newsletter'
+import Testimonial from './Testimonial'
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Testimonial/>
     <Contact/>
     <Newsletter/>
     <Footer/>
