c0c735ee7ea1a8d40fa63977fe390258f92e9967
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-05 17:36:19 +0600

    Configuration Files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import Contact from './Contact'
-import Footer from './Footer'
-import Navbar from './Navbar'
-import Newsletter from './Newsletter'
-import Testimonial from './Testimonial'
+import Contact from './Contact';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Newsletter from './Newsletter';
+import Testimonial from './Testimonial';
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    <Testimonial/>
-    <Contact/>
-    <Newsletter/>
-    <Footer/>
+      <Navbar />
+      <Testimonial />
+      <Contact />
+      <Newsletter />
+      <Footer />
     </>
-  )
+  );
 }
