9651573d25f489e0806268479bfdbfbe61f7e218
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-10 01:01:32 +0600

    DOM property fixed

--- pages/Partner/index.tsx
+++ pages/Partner/index.tsx
@@ -22,9 +22,9 @@ const Partner = () => {
               xmlns='http://www.w3.org/2000/svg'
             >
               <path
-                fill-rule='evenodd'
+                fillRule='evenodd'
                 d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
-                clip-rule='evenodd'
+                clipRule='evenodd'
               ></path>
             </svg>
           </a>
@@ -48,9 +48,9 @@ const Partner = () => {
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                   d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-                  clip-rule='evenodd'
+                  clipRule='evenodd'
                 ></path>
               </svg>
             </a>
