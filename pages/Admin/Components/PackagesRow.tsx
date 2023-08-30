0ad2ed3373e38c5161e7a9877ad931907269335b
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-30 08:30:47 +0600

    Admin/PackageRow #Comment

--- pages/Admin/Components/PackagesRow.tsx
+++ pages/Admin/Components/PackagesRow.tsx
@@ -1,38 +1,39 @@
-// import React from 'react';
+import React from 'react';
 
-// interface Package {
-//   PackageId: number;
-//   Name: string;
-//   ValidFrom: Date;
-//   ValidTill: Date;
-//   PackageType: string;
-//   TransportFacility: string;
-// }
+interface Package {
+  PackageId: number;
+  Name: string;
+  ValidFrom: Date;
+  ValidTill: Date;
+  PackageType: string;
+  TransportFacility: string;
+}
 
-// interface PackageRowProps {
-//   packageItem: {
-//     PackageId: number;
-//     Name: string;
-//     ValidFrom?: Date; // Optional ValidFrom
-//     ValidTill?: Date; // Optional ValidTill
-//     PackageType: string;
-//     TransportFacility: string;
-//   };
-// }
+interface PackageRowProps {
+  packageItem: {
+    PackageId: number;
+    Name: string;
+    ValidFrom?: Date; // Optional ValidFrom
+    ValidTill?: Date; // Optional ValidTill
+    PackageType: string;
+    TransportFacility: string;
+  };
+}
 
-// const PackageRow: React.FC<PackageRowProps> = ({ packageItem }) => {
-//   return (
-//     <tr key={packageItem.PackageId}>
-//       <td>{packageItem.Name}</td>
-//       <td>{packageItem.ValidFrom.toLocaleDateString()}</td>{' '}
-//       {/* Convert to a formatted string */}
-//       <td>{packageItem.ValidTill.toLocaleDateString()}</td>{' '}
-//       {/* Convert to a formatted string */}
-//       <td>{packageItem.PackageType}</td>
-//       <td>{packageItem.TransportFacility}</td>
-//       {/* Add more columns as needed */}
-//     </tr>
-//   );
-// };
+const PackageRow: React.FC<PackageRowProps> = ({ packageItem }) => {
+  return (
+    <div></div>
+    // <tr key={packageItem.PackageId}>
+    //   <td>{packageItem.Name}</td>
+    //   <td>{packageItem.ValidFrom.toLocaleDateString()}</td>{' '}
+    //   {/* Convert to a formatted string */}
+    //   <td>{packageItem.ValidTill.toLocaleDateString()}</td>{' '}
+    //   {/* Convert to a formatted string */}
+    //   <td>{packageItem.PackageType}</td>
+    //   <td>{packageItem.TransportFacility}</td>
+    //   {/* Add more columns as needed */}
+    // </tr>
+  );
+};
 
-// export default PackageRow;
+export default PackageRow;
