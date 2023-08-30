ba8243e57ddc1ef2ef07897686fe18542559cf06
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-30 09:12:53 +0600

    All Axios Changes #2

--- pages/utils/axiosInstance.tsx
+++ pages/utils/axiosInstance.tsx
@@ -4,4 +4,16 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000/',
 });
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Handle error here
+    console.error('Axios request error:', error);
+
+    // You can throw a custom error here or return a rejected Promise
+    // to propagate the error to the code that uses axiosInstance
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
