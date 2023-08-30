6ad40176f48439dfccfd971ff805043ccdcb27b9
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-08-30 18:45:35 +0600

    //

--- pages/ProtectedRoute.tsx
+++ pages/ProtectedRoute.tsx
@@ -3,9 +3,23 @@ import { useRouter } from 'next/router';
 import useFetchUserData from './api/User/useFetchUserData';
 import Cookies from 'js-cookie';
 const ProtectedRoute = ({ allowedRoles, children }: any) => {
-  const router = useRouter();
-  const userData = useFetchUserData();
-  const token = Cookies.get('token');
+  // const router = useRouter();
+  // const userData = useFetchUserData();
+  // const token = Cookies.get('token');
+
+  // useEffect(() => {
+  //   if (!token) {
+  //     router.push('/404');
+  //   } else if (!userData) {
+  //     router.push('/login');
+  //   } else if (!allowedRoles.includes('admin')) {
+  //     router.push('/404');
+  //   }
+  // }, [token, userData, allowedRoles, router]);
+
+  // if (!userData || !allowedRoles.includes('admin')) {
+  //   return null;
+  // }
 
   return <>{children}</>;
 };
